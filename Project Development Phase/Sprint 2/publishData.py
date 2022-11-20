90fb1045837d54c4fbde48d7c7785c6287e6b39b
Sweswetha1402 <113255627+Sweswetha1402@users.noreply.github.com>
2022-11-20 18:51:35 +0530

    Update publishData.py

--- Project Development Phase/Sprint 2/publishData.py
+++ Project Development Phase/Sprint 2/publishData.py
@@ -4,6 +4,7 @@
 
 import wiotp.sdk.device # python -m pip install wiotp
 import time
+import random
 
 # IMPORT SECTION ENDS
 # -----------------------------------------------
@@ -28,14 +29,44 @@ def myCommandCallback(cmd):
     print("recieved cmd : ",cmd)
 
 
-def logData2Cloud(location,temperature,visibility):
+def logData2Cloud(location,temperature,visibility,humidity):
     client = wiotp.sdk.device.DeviceClient(config=myConfig,logHandlers=None)
     client.connect()
+    repo=random.randint(0,5)
+    if repo==1:
+        prt="SLOW DOWN , SCHOOL IS NEAR"
+    elif repo==3:
+        prt="SLOW DOWN , HOSPITAL NEARBY"
+    elif repo==5:
+        prt="NEED HELP, POLICE STATION NEARBY"
+    else:
+        prt=""
+    speed=random.randint(0,150)
+    if speed>=100:
+        prt3="SLOW DOWN , Speed Limit Exceeded"
+    elif speed>=60 and speed<100:
+        prt3="Moderate Speed"
+    else:
+        prt3="Usual speed limit"
+    sign=random.randint(0,5)
+    if sign==1:
+        prt2="Right Diversion ->"
+    elif sign==3:
+        prt2="Left Diversion <-"
+    elif sign==5:
+        prt2="U Turn"
+    else:
+        prt2=""
+    if temperature<=50:
+        prt4="Fog Ahead, Drive Slow"
+    else:
+        prt4="Clear Weather"
+
     client.publishEvent(eventId="status",msgFormat="json",data={
         "temperature" : temperature,
-        "speedlimit" : visibility,
+        "speedlimit" : visibility,"humidity":humidity,"Message":prt, "Sign":prt2, "Speed":prt3, "Visibility":prt4,
         "location" : location
-    },qos=0,onPublish=None)
+    },qos=0,onPublish=None)  
     client.commandCallback = myCommandCallback
     client.disconnect()
 
